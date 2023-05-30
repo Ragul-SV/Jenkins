c6ea3712600c2c3ddd123f67e82b162cd3b51bf6
Ragul S V <ragulsv@Raguls-MacBook-Air.local>
2023-05-30 19:43:34 +0530

    added node js docker groovy file

---
+++ nodejsdocker.groovy
@@ -0,0 +1,25 @@
+job('NodeJS Docker example') {
+    scm (
+        git('git://github.com/wardviane/docker-demo.git') { node ->
+            node / gitConfigName('DSL User')
+            node / gitConfigMail('jenkins-dsl@newtech.academy')
+        }
+    )
+}
+triggers {
+    scm('H/5 * * * *')
+}
+wrappers {
+    nodejs('nodejs')
+}
+steps {
+    dockerBuildAndPublish {
+        repositoryName('ragulsv1999/docker-nodejs-demo')
+        tag('$(GIT_REVISION,length=9)')
+        registryCredentials('dockerhub')
+        forcePull(false)
+        forceTag(false)
+        createFingerprints(false)
+        skipDecorate()
+    }
+}
